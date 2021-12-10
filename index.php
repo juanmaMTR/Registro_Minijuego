8d1c8dd5ee112cc282f46b3cb135a847c3ab3dd8
juanmaMTR <jtoscanoreyes.guadalupe@alumnado.fundacionloyola.net>
2021-12-10 14:18:47 +0100

    Script hecho, index en proceso

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+    <html lang="es">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="author" content="<jtoscanoreyes.guadalupe@alumnado.fundacionloyola.net>">
+        <title>Registro</title>
+    </head>
+    <body>
+        <h1>REGISTRO</h1>
+        <form action="form" method="post">
+            <input type="text" id="nombreUsuario" placeholder="Username" />
+            <input type="text" id="contrasena" placeholder="Password" />
+            <input type="text">
+        </form>
+    </body>
+</html>
---
+++ sql/script.sql
@@ -0,0 +1,32 @@
+--Se crea la base de datos de Minijuegos
+CREATE DATABASE BD_Minijuegos;
+--Usamos la base de datos que hemos creado
+USE BD_Minijuegos;
+--Creamos la tabla Usuario
+CREATE TABLE Usuario(
+    idUsuario smallint UNSIGNED AUTO_INCREMENT  NOT NULL PRIMARY KEY,
+    nombreUsuario varchar(60) NOT NULL,
+    contraseña varchar(50) NOT NULL,
+    tipo char(1)
+);
+--Creamos la tabla Minijuegos
+CREATE TABLE Minijuegos(
+    idMinijuego smallint UNSIGNED AUTO_INCREMENT  NOT NULL PRIMARY KEY,
+    nombreMinijuego varchar(60) NOT NULL,
+    url varchar(100) NOT NULL
+);
+--Creamos la tabla Preferencias que sale de una relación M:N
+CREATE TABLE Preferencias(
+    idUsuario smallint UNSIGNED  NOT NULL ,
+    idMinijuego smallint UNSIGNED  NOT NULL ,
+    CONSTRAINT FK_idUsuario FOREIGN KEY idUsuario REFERENCES Usuario(idUsuario),
+    CONSTRAINT FK_idMinijuego FOREIGN KEY idMinijuego REFERENCES Minijuegos(idMinijuego),
+    CONSTRAINT PK_preferencias PRIMARY KEY (idUsuario,idMinijuego)
+);
+--INSERT INTO--
+INSERT INTO Minijuegos(nombreMinijuego,url) 
+VALUES  ('Tabla Periodica','/minijuegos/tablaPeriodica'),
+        ('Flash Cards','/minijuegos/flashCards'),
+        ('Ligas','/minijuegos/ligas'),
+        ('Crazy Ball','/minijuegos/crazyBall'),
+        ('Reciclaje','/minijuegos/reciclaje');
